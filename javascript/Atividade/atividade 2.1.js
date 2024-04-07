46318378e05dba9b646a7d5a142dec1d26cb9b72
Marcos Gabriel Paz <117696460+Marcaogabriel@users.noreply.github.com>
2024-04-06 23:27:55 -0300

    Delete javascript directory

--- javascript/Atividade/atividade 2.1.js
+++
@@ -1,9 +0,0 @@
-let numero = 42
-
-if(numero % 2 !== 0){
-    console.log(`o número ${numero} é impar`)
-}
-
-else{
-    console.log(`o número ${numero} é par`)
-}
--- javascript/Atividade/atividade 3.3.js
+++
@@ -1,6 +0,0 @@
-// Fatorial
-fatorial = 1
-for(i = 1; i <= 5; i++){
-    fatorial *=  i
-}
-console.log(` 5! =  5 x 4 x 3 x 2 x 1 = ${fatorial}`)
