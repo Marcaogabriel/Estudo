67f01baa2dad2a82e53521fc29d2d9f24f2cd5b5
Marcos Gabriel Paz <mgabrielpazdeoliveira@gmail.com>
2024-05-04 10:00:23 -0600

    Pr

---
+++ javascript/React/Projeto/projeto/src/components/index.js
@@ -0,0 +1,4 @@
+import Input from "./Input/Input";
+import Button from "./Button/Button";
+import Header from "./header"
+export { Input, Button, Header  }
---
+++ javascript/React/Projeto/projeto/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider ,createBrowserRouter} from 'react-router-dom';
+import Home from './pages/Home';
+import ErrorPage from './pages/errorpage';
+import Item from './pages/item';
+import Opcoes from './pages/opcoes';
+
+const routes = createBrowserRouter([
+  {
+    path: "/", // caminho
+    element: <Home/>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/opcoes",
+    element: <Opcoes/>
+  },
+  {
+    path: "/item",
+    element: <Item/>
+  }, 
+
+])
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<RouterProvider router={routes}/>)
+
+
